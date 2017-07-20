d29aef101fbde1c71b3070b3d7e5274560959500
Charles Wooley <cwooley89@gmail.com>
2017-07-20 14:16:25 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+
+
+
+let formEvent = function (event) {
+  let itemTxt = $('#item').val()
+  let myLi = document.createElement("LI");
+  myLi.innerText = itemTxt
+  $('#list ol').append(myLi)
+  event.preventDefault();
+}
+
+submitForm = function () {
+  $('form').on('submit', submitForm);
+}
